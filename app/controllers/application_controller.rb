988d2d9441c7de2505950f342876894ace40cfaf
Giovani Fernandes <giovani_oli@hotmail.com>
2022-06-11 16:51:39 -0400

    feat: config i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    I18n.with_locale(locale_from_header, &action)
+  end
+
+  private
+
+  def locale_from_header
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    'pt' == locale ? 'pt-BR' : locale
+  end
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,3 @@
+I18n.available_locales = [:en, :es, :'pt-BR']
+
+I18n.default_locale = :en
