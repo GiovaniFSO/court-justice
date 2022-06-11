3e2206b427a14aa1998d6772668c9774ec69ea97
Giovani Fernandes <giovani_oli@hotmail.com>
2022-06-11 17:02:50 -0400

    refactor: switch locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
 
-  def switch_locale(&action)
-    I18n.with_locale(locale_from_header, &action)
+  def switch_locale(&)
+    I18n.with_locale(locale_from_header, &)
   end
 
   private
 
   def locale_from_header
     locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-    'pt' == locale ? 'pt-BR' : locale
+    locale == 'pt' ? 'pt-BR' : locale
   end
 end
